cafa2d18b62fd8b2fffbc2209420ef525b6d7aff
AVVKavvk <kumawatvipin066@gmail.com>
2024-10-25 15:56:41 +0530

    added model

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/LMS/utils"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	router := echo.New()
 
-	router.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading environment variables file")
+	}
+	PORT := os.Getenv("PORT")
+
+	route := echo.New()
+
+	route.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format:           `${time_custom} [INFO] [<NONE>, ${path}]: {"http_method": "${method}", "http_ip": "${remote_ip}", "http_status": ${status}, "http_latency": ${latency}, "http_agent": "${user_agent}", "http_error": "${error}", "http_path": "${path}", "http_bytes_in": ${bytes_in}, "http_bytes_out": ${bytes_out}}` + "\n",
 		CustomTimeFormat: "20060102150405.000",
 		Output:           os.Stdout,
 	}))
-	router.Use(middleware.RequestID())
+	route.Use(middleware.RequestID())
 
-	router.GET("/", func(ctx echo.Context) error {
+	route.GET("/", func(ctx echo.Context) error {
 
-		// return utils.Success(ctx, http.StatusOK, "Hello World", nil)
-		return utils.Error(ctx, http.StatusBadRequest, "Hello World")
+		return utils.Success(ctx, http.StatusOK, "Hello World", nil)
+		// return utils.Error(ctx, http.StatusBadRequest, "Hello World")
 	})
 
-	router.Start(":8080")
+	fmt.Println("Server is running on port: ", PORT)
+	route.Start(":" + PORT)
 }
