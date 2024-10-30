89c8c9fc68d23a37601a414f7b06f1742e441ffa
Vipin Kumawat <96615928+AVVKavvk@users.noreply.github.com>
2024-10-31 00:32:32 +0530

    done with book (#5)

--- model/book.go
+++ model/book.go
@@ -3,12 +3,30 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Book struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
-	BookId   string             `json:"bookId,omitempty" bson:"bookId,omitempty"`
-	Course   string             `json:"course,omitempty" bson:"course,omitempty"`
-	Sem      string             `json:"sem,omitempty" bson:"sem,omitempty"`
-	Count    int                `json:"count,omitempty" bson:"count,omitempty"`
-	Penality float32            `json:"penality,omitempty" bson:"penality,omitempty"`
-	Student  []string           `json:"student,omitempty" bson:"student,omitempty"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	BookId    string             `json:"bookId,omitempty" bson:"bookId,omitempty"`
+	Course    string             `json:"course,omitempty" bson:"course,omitempty"`
+	Sem       string             `json:"sem,omitempty" bson:"sem,omitempty"`
+	Count     int                `json:"count,omitempty" bson:"count,omitempty"`
+	Penality  float32            `json:"penality,omitempty" bson:"penality,omitempty"`
+	Students  []string           `json:"students,omitempty" bson:"students,omitempty"`
+	CreatedAt primitive.DateTime `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt primitive.DateTime `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+}
+
+func (book *Book) IsAllFieldEmpty() bool {
+	if book.BookId == "" || book.Count == 0 || book.Name == "" || book.Course == "" || book.Sem == "" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (book *Book) IsVaildCount() bool {
+	if book.Count <= 0 {
+		return false
+	} else {
+		return true
+	}
 }
--- model/issued.go
+++ model/issued.go
@@ -8,8 +8,8 @@ type Issued struct {
 	Sem         string             `json:"sem,omitempty" bson:"sem,omitempty"`
 	BookId      string             `json:"bookId,omitempty" bson:"bookId,omitempty"`
 	StudentId   string             `json:"studentId,omitempty" bson:"studentId,omitempty"`
-	IssueDate   string             `json:"issueDate,omitempty" bson:"issueDate,omitempty"`
-	ReturnDate  string             `json:"returnDate,omitempty" bson:"returnDate,omitempty"`
+	IssueDate   primitive.DateTime `json:"issueDate,omitempty" bson:"issueDate,omitempty"`
+	ReturnDate  primitive.DateTime `json:"returnDate,omitempty" bson:"returnDate,omitempty"`
 	Penality    float32            `json:"penality,omitempty" bson:"penality,omitempty"`
 	BookName    string             `json:"bookName,omitempty" bson:"bookName,omitempty"`
 	StudentName string             `json:"studentName,omitempty" bson:"studentName,omitempty"`
