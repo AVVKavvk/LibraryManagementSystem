8080473c25b81611ce05f08d6eeb7a8efb95b658
AVVKavvk <kumawatvipin066@gmail.com>
2024-11-01 15:23:51 +0530

    added some changes

--- router/router.go
+++ router/router.go
@@ -16,17 +16,16 @@ func RegisterRoutes(e *echo.Echo) {
 	admin.PUT("/forget", api.AdminForgetPassword)
 	admin.DELETE("", api.DeleteAdmin)
 
-	
 	admin.GET("/student/:mis", api.GetStudentWithoutPassword)
 	admin.POST("/assign/:mis/:bookId", api.AddBookToStudent)
 	
 	admin.POST("/book", api.CreateBook)
-	admin.GET("/books", api.GetAllBooks)
 	admin.PUT("/book/:bookId", api.UpdateBookCount)
 	admin.PUT("/assign", api.AssignBookToStudent)
 	admin.DELETE("/delete", api.DeleteBookFromStudent)
 	
-	student := e.Group("/student")
+
+	student := e.Group("/student", middleware.IsAuthorized())
 	student.POST("", api.CreateStudent)
 	student.PUT("/name-phone", api.UpdateStudentNamePhone)
 	student.PUT("/password", api.UpdateStudentPassword)
@@ -34,14 +33,23 @@ func RegisterRoutes(e *echo.Echo) {
 	student.GET("/dues-penality/:mis", api.GetStudentPenalityDues)
 	student.GET("/books/:mis", api.GetBooksAssociateWithStudent)
 	
-	book := e.Group("/book")
+	book := e.Group("/book", middleware.IsAuthorized())
 	book.GET("/:bookId", api.GetBookByID)
 	book.GET("/student/:bookId", api.GetAllMISAssociateWithBook)
 	book.GET("/course/:course", api.GetBooksByCourse)
 	book.GET("/:course/:sem", api.GetBooksBySemWithCourse)
 	
-	e.POST("/login/admin", api.GetAdmin)
-	e.GET("/admin-profile/:id",api.GetAdminByID)
+
+	all:=e.Group("/",middleware.IsAuthorized())
+	all.POST("login/admin", api.GetAdmin)
+	all.GET("admin-profile/:id", api.GetAdminByID)
+	all.GET("books", api.GetAllBooks)
+
+	// e.POST("/login/admin", api.GetAdmin)
+	// e.GET("/admin-profile/:id",api.GetAdminByID, middleware.IsAdmin())
+	// e.GET("/books", api.GetAllBooks,middleware.IsAuthorized())
+
+
 	e.GET("/", func(ctx echo.Context) error {
 		return ctx.File("./index.html")
 
